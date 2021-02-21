71a4e96497ade174b3f52b6206510515e986faf7
Alex <asko1205@gmail.com>
2021-02-21 14:38:10 -0800

    A real good lesson in React Forms and Custom Hooks

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  // this is reactive, so when updated it'll update anywhere it's used
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
+    name: 'Nice shoes',
+    price: 12312,
+    description: 'These are the best shoes',
+  });
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description
+        <input
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Description"
+          value={inputs.description}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
+    </form>
+  );
+}
---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // custom hook!
+  // create a state object for our inputs
+  //   {
+  //       name: 'wes',
+  //       description: 'nice shoes',
+  //       price: 1000
+  //   }
+  const [inputs, setInputs] = useState(initial);
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      // funky way file upload works
+      value[0] = e.target.files;
+    }
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
