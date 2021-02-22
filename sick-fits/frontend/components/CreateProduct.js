932af9bed211954fd598e2863dc211c469baf43e
Alex <asko1205@gmail.com>
2021-02-21 17:18:52 -0800

    Creating products via mutations

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,6 +1,34 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import { useState } from 'react';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in? and what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   // this is reactive, so when updated it'll update anywhere it's used
@@ -10,24 +38,33 @@ export default function CreateProduct() {
     price: 12312,
     description: 'These are the best shoes',
   });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+        // submit the inputfields to the backend
+        await createProduct();
+        clearForm();
       }}
     >
-      <label htmlFor="image">
-        Image
-        <input
-          required
-          type="file"
-          id="image"
-          name="image"
-          onChange={handleChange}
-        />
-      </label>
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
         <label htmlFor="name">
           Name
           <input
